56c5624fa853ec27315d9624ab2a911a1d202b2a
Mike Shea <michael.shea@cio.ny.gov>
2012-10-09 14:31:12 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuseApp::Application.config.secret_token = '1a314f218d9c9083fedeb1c8e394a60681bf88d82f5f0285b62391c5c9ca045ce5f0877742257073b5c49285e2b8231233e9117de77ec11a58d08d8a5b26c927'
